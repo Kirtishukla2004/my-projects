4ef5b13f0f927d279f557febe9f1474707b40df7
kirti shukla <104082719+shukla2004@users.noreply.github.com>
2022-06-15 16:06:50 +0530

    FINDING SQUARE OF A NUMBER IN C CODE

---
+++ square.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int i,s;
+	printf("square of numbers 1-5");
+	for(i==0;i<=5;i++)
+	{
+	
+		s=i*i;
+		printf(" \n %d*%d",i,s);
+	}
+		
+	
+	getch();
+}
