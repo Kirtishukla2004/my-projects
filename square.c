3c3deb768d723ced6e789ad3cd345b5e6a230ee4
kirti shukla <104082719+shukla2004@users.noreply.github.com>
2022-06-15 16:21:45 +0530

    square of any number by taking input from user in c code

--- square.c
+++ square.c
@@ -1,16 +1,16 @@
-#include<stdio.h>
-#include<conio.h>
+#include <stdio.h>
+#include <conio.h>
 void main()
 {
-	int i,s;
-	printf("square of numbers 1-5");
-	for(i==0;i<=5;i++)
+	int i, s, n;
+	printf("enter the number: ");
+	scanf("%d", &n);
+	for (i = 0; i <= n; i++)
 	{
-	
-		s=i*i;
-		printf(" \n %d*%d",i,s);
+
+		s = i * i;
 	}
-		
-	
+	printf(" square of the number:%d", s);
+
 	getch();
 }
